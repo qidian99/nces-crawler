3c8df5d69d54103fd26699fa1f9794406dcd3b34
含风 <qidian.qd@alibaba-inc.com>
2021-08-12 04:00:05 +0800

    Demoed

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# SchoolDistrictsByState
+# NCES Crawler
 
-Automation Script for Fetching all NCES School Districts by States
+Automation Script for Fetching all NCES School Districts by States.
 
 ## URL
 https://nces.ed.gov/ccd/districtsearch/district_list.asp?Search=1&details=1&InstName=&DistrictID=&Address=&City=&State=02&Zip=&Miles=&County=&PhoneAreaCode=&Phone=&DistrictType=1&DistrictType=2&DistrictType=3&DistrictType=4&DistrictType=5&DistrictType=6&DistrictType=7&DistrictType=8&NumOfStudents=&NumOfStudentsRange=more&NumOfSchools=&NumOfSchoolsRange=more
--- index.js
+++ index.js
@@ -20,44 +20,67 @@ var driver = new webdriver.Builder()
 
 const mockURL = 'https://nces.ed.gov/ccd/districtsearch/district_list.asp?Search=1&details=1&InstName=&DistrictID=&Address=&City=&State=02&Zip=&Miles=&County=&PhoneAreaCode=&Phone=&DistrictType=1&DistrictType=2&DistrictType=3&DistrictType=4&DistrictType=5&DistrictType=6&DistrictType=7&DistrictType=8&NumOfStudents=&NumOfStudentsRange=more&NumOfSchools=&NumOfSchoolsRange=more';
 
-(async () => {
+async function NCESQueryByState(url = null, urls = []) {
 
-    await driver.get(mockURL);
+    try {
 
-    await driver.findElement(By.css('a[href="JavaScript:GetExcelFile();"]')).click();
-
-    // driver.switchTo().newWindow
-    const parentWindowHandler = await driver.getWindowHandle(); // Store your parent window
-    let subWindowHandler = null;
-
-    console.log('parentWindowHandler', parentWindowHandler);
-
-    const handles = await driver.getAllWindowHandles(); // get all window handles
-    console.log(handles);
-
-    handles.forEach((handle) => {
-        if (handle !== parentWindowHandler && subWindowHandler === null) {
-            subWindowHandler = handle;
+        for(const u of urls) {
+            await NCESQueryByState(u);
         }
 
-    })
+        if (url === null) {
+            url = mockURL;
+        }
 
-    console.log('subWindowHandler', subWindowHandler);
+        console.log("Getting: ", url);
 
-    // driver.switchTo().window(subWindowHandler); // switch to popup window
+        await driver.get(url);
 
-    // Now you are in the popup window, perform necessary actions here
-    if (subWindowHandler === null) {
-        return;
+        await driver.findElement(By.css('a[href="JavaScript:GetExcelFile();"]')).click();
+
+        // driver.switchTo().newWindow
+        let parentWindowHandler = await driver.getWindowHandle(); // Store your parent window
+        let subWindowHandler = null;
+
+        console.log('parentWindowHandler', parentWindowHandler);
+
+        const handles = await driver.getAllWindowHandles(); // get all window handles
+        console.log(handles);
+
+        handles.forEach((handle) => {
+            if (handle !== parentWindowHandler && subWindowHandler === null) {
+                subWindowHandler = handle;
+            }
+
+        })
+
+        console.log('subWindowHandler', subWindowHandler);
+
+        // driver.switchTo().window(subWindowHandler); // switch to popup window
+
+        // Now you are in the popup window, perform necessary actions here
+        if (subWindowHandler === null) {
+            return;
+        }
+
+        await driver.switchTo().window(subWindowHandler);
+
+        // await driver.findElement(By.css('a[href="JavaScript:GetExcelFile();"]')).click();
+
+        let firstResult = await driver.wait(until.elementLocated(By.css('td[align=right] > a[target^=NCESExcelTarget]')), 10000);
+        console.log(await firstResult.getAttribute('textContent'));
+
+        await firstResult.click();
+
+        await driver.close();
+
+        await driver.switchTo().window(parentWindowHandler);
+        parentWindowHandler = null;
+        console.log("Finished. Driver Closed.");
+    } catch (e) {
+        console.error(e);
     }
+}
 
-    await driver.switchTo().window(subWindowHandler);
 
-    // await driver.findElement(By.css('a[href="JavaScript:GetExcelFile();"]')).click();
-
-    let firstResult = await driver.wait(until.elementLocated(By.css('td[align=right] > a[target^=NCESExcelTarget]')), 10000);
-    console.log(await firstResult.getAttribute('textContent'));
-
-    firstResult.click();
-
-})()
+NCESQueryByState(null, queries);
